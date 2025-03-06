33b2406a2111eccbaa5e565f113d0c0867316516
Michal Raus <raus.michal@email.cz>
2025-03-06 18:10:27 +0100

    Polozeni zakladni struktury aplikace ROUTER - Store a Cart

---
+++ src/views/Cart.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+// import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <p>Já jsem stránka Cart</p>
+  </main>
+</template>
+
+<style>
+
+</style>
---
+++ src/views/Store.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+// import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  
+    <p>Ahoj já jsem Store</p>
+ 
+</template>
+
+<style>
+
+</style>
